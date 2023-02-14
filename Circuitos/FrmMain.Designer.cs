247d5cb7649ebac7e1059800d4c1088b070a67db
Ahnuar <73461940+Ahnuar@users.noreply.github.com>
2023-02-14 12:25:37 +0100

    Exportacion de base de datos

---
+++ Circuitos/FrmMain.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Circuitos
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dadesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.circuitosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dadesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1078, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dadesToolStripMenuItem
+            // 
+            this.dadesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.circuitosToolStripMenuItem});
+            this.dadesToolStripMenuItem.Name = "dadesToolStripMenuItem";
+            this.dadesToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.dadesToolStripMenuItem.Text = "Dades";
+            // 
+            // circuitosToolStripMenuItem
+            // 
+            this.circuitosToolStripMenuItem.Name = "circuitosToolStripMenuItem";
+            this.circuitosToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.circuitosToolStripMenuItem.Text = "Circuitos";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 670);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dadesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem circuitosToolStripMenuItem;
+    }
+}
+
